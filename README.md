0b666f39a59f5f6219473ae36e6b41195b739ad1
adricen <adrien.centonze@gmail.com>
2019-08-20 08:26:34 +0200

    init web gl context

---
+++ README.md
@@ -0,0 +1,7 @@
+# WebGL Getting started!
+
+leçon sur MDN : [MDN-webGL](https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Adding_2D_content_to_a_WebGL_context)
+Wiki sur Texels : [texel graphics](https://en.wikipedia.org/wiki/Texel_(graphics))
+[Dev opera](https://dev.opera.com/articles/introduction-to-webgl-part-1/)
+[Philo render motor](http://www.senchalabs.org/philogl/demos.html#lessons)
+[Three.js](https://threejs.org/)
---
+++ scene/assets/js/script.js
@@ -0,0 +1,28 @@
+main();
+
+function main() {
+  const canvas = document.querySelector("#glCanvas");
+  // Initialize the GL context
+  const gl = canvas.getContext("webgl");
+
+  // Only continue if WebGL is available and working
+  if (gl === null) {
+    alert("unable to initialize WebGL. Your browser or machine may not support it.");
+    return;
+  }
+
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+// Vertx Shader program
+const vsSource = attribute vec4 aVertexPosition;
+
+  uniform mat4 uModelViewMatrix;
+  uniform mat4 uProjectionMatrix;
+
+  void main() {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+  }
